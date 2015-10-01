592c2d3041482dc6c3f257de64df412264f84a4c
Bogdan Cernat <cernat.bogdan.stefan@gmail.com>
2015-10-01 11:40:34 +0300

    remove unused file, remove trailing spaces from app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var app = (function (root) {
         layers = [];
         usedCenters = [];
     };
-    
+
     x.drawCircle = function (coords, radius) {
 //        color = (new root.RColor()).get(false, 0.739, 0.54);
 
@@ -37,30 +37,30 @@ var app = (function (root) {
         context.fill();
         context.closePath();
     };
-    
+
     x.animate = function () {
         root.tb.animationFrame(this.animate);
         this.draw();
     };
-    
+
     x.draw = function () {
         var splitsAngle = 15,
             splits = 360 / splitsAngle,
             i,
             j,
             layer;
-        
+
         layers = [];
         usedCenters = [];
         /* add root circle */
         layers.push([{c: center, r: radius}]);
         usedCenters.push(center);
-        
+
         /* init first-layer */
         layer = [];
         layer.push({c: root.tb.getCoordsOnCircle(0, radius, center), r: radius});
         usedCenters.push(root.tb.getCoordsOnCircle(0, radius, center));
-        
+
         for (i = 0; i < 5; i += 1) {
             var candidat = this.getNextCircleCenter(layers[0][0], layer[layer.length - 1]);
             layer.push({
@@ -81,12 +81,12 @@ var app = (function (root) {
                     b = layers[layers.length - 1][(i + 1) % nrOfCircles],
                     c = null,
                     candidat = null;
-                
+
                 if (j == 2) {
                     a = layers[layers.length - 2][i];
                     b = layers[layers.length - 1][i];
                     c = layers[layers.length - 2][(i + 1) % 6]
-                    
+
                     candidat = this.getNextCircleCenter(c, b);
 
                     layer2.push({
@@ -103,14 +103,14 @@ var app = (function (root) {
                 });
                 usedCenters.push(candidat);
             }
-            
+
             layers.push(layer);
             if(layer2.length) {
                 layers.push(layer2);
             }
 
         }
-        
+
         root.console.log(layers);
         for (i = 0; i < layers.length; i += 1) {
             for (j = 0; j < layers[i].length; j += 1) {
@@ -118,7 +118,7 @@ var app = (function (root) {
             }
         }
     };
-    
+
     x.getNextCircleCenter = function (C1, C2) {
         var d = root.tb.distAB(C1.c, C2.c),
             delta = null,
@@ -129,29 +129,29 @@ var app = (function (root) {
             p1 = null,
             p2 = null;
         delta = (1 / 4) * Math.sqrt((d + C1.r + C2.r) * (d + C1.r - C2.r) * (d - C1.r + C2.r) * (-d + C1.r + C2.r));
-        
+
         x_a = (C1.c.x + C2.c.x) / 2 + ((C2.c.x - C1.c.x) * (Math.pow(C1.r, 2) - Math.pow(C2.r, 2)) / (2 * Math.pow(d, 2)));
         x_b = 2 * ((C1.c.y - C2.c.y) / Math.pow(d, 2)) * delta;
-        
+
         y_a = (C1.c.y + C2.c.y) / 2 + ((C2.c.y - C1.c.y) * (Math.pow(C1.r, 2) - Math.pow(C2.r, 2)) / (2 * Math.pow(d, 2)));
         y_b = 2 * ((C1.c.x - C2.c.x) / Math.pow(d, 2)) * delta;
-        
+
         p1 = {
             x: x_a - x_b,
             y: y_a + y_b
         };
-        
+
         p2 = {
             x: x_a + x_b,
             y: y_a - y_b
         };
-        
+
         if (this.isUsedCircleCenter(p1)) {
             return p2;
         }
         return p1;
     };
-    
+
     x.isUsedCircleCenter = function (point) {
         var i = null,
             c = null;
@@ -163,7 +163,7 @@ var app = (function (root) {
         }
         return false;
     };
-    
+
     return x;
 }(window));
 
